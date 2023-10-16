8d15dfaaae19824cfb1202c4aba91e223980e5af
EdogawaCoTrung <quangtrungproki@gmail.com>
2023-10-16 14:28:48 +0700

    update1

--- template3.js
+++ template3.js
@@ -5,6 +5,8 @@ window.addEventListener('scroll', box_fly_in);
 function box_fly_in(){
     const TriggerBottom1 = window.innerHeight / 5 * 4;
     const boxTop = boxes_fly_in.getBoundingClientRect().top;
+    console.log("vi tri cua window1: "+TriggerBottom1);
+        console.log("vi tri cua item1: " + boxTop);
     if (boxTop < TriggerBottom1) {
         boxes_fly_in.classList.add('show_fly_in');
     }
@@ -12,6 +14,23 @@ function box_fly_in(){
         boxes_fly_in.classList.remove('show_fly_in');
 }
 
+const models_fly_in = document.querySelectorAll('.model-fly-in');
+
+window.addEventListener('scroll', model_get_fly_in);
+
+function model_get_fly_in() {
+    const TriggerBottom3 = window.innerHeight * 5 / 4;
+    console.log("chieudai cua window " + TriggerBottom3);
+    models_fly_in.forEach((modelitem) => {
+        const getitemtop = modelitem.getBoundingClientRect().top;
+        console.log("top cua item " + getitemtop);
+        if (getitemtop < TriggerBottom3) {
+            modelitem.classList.add('show_model_fly_in');
+        }
+        else
+            modelitem.classList.remove('show_model_fly_in');
+    });
+}
 const text_fly_in = document.querySelectorAll('.about-us-slider');
 
 window.addEventListener('scroll', about_us_fly_in);
@@ -25,7 +44,7 @@ function about_us_fly_in() {
         }
         else
             text_top.classList.remove('show_text_fly_in');
-    })
+    });
 }
 
 let DisplayValues = document.querySelectorAll('.num');
